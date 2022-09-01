314bcbb9b9d39758f0a83571363a64f20dc38cf9
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-09-01 12:47:50 +0900

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Header from "./Components/Header"
 
 function App() {
-  return (<>
-    <Header/>
-  </>)
+  return (
+    <>
+      <Header/>
+    </>
+  )
 }
 
 export default App;
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    font-size: 25px;
+    font-weight: 600;
     background: whitesmoke;
     border-bottom: 1px solid rgba(0,0,0,0.1);
+    height: 50px;
+    min-width: 480px;
 `
 
 const Header = () => {
