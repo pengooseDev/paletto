53a996c0abe13d63926e7124b199685c2fac09d1
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-09-05 15:54:13 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
 import Header from "./Components/Header";
-import Color from "./Components/Color";
+import GenerateColorBox from "./Components/GenerateColor";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    height: 100vh;
+    background: teal;
+    display: flex;
+    flex-direction: column;
+`
 
 function App() {
     return (
         <>
-            <Header />
-            <Color />
+            <Wrapper>
+                <Header />
+                <GenerateColorBox />
+            </Wrapper>
         </>
     );
 }
